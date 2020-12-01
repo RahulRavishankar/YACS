3b0532d429fc89a6b7d5138d8a05ba80c5f445d4
gaurika poplai <gaurikapoplai@gmail.com>
2020-12-01 14:24:41 +0530

    send_tasks function from master to worker

--- master.py
+++ master.py
@@ -9,6 +9,12 @@ import threading
 tasks_queue = []
 
 
+def send_tasks(data, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(('localhost', port))
+    s.send(data.encode())
+
+
 def listen_to_requests():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind(('localhost', 5000))
@@ -41,57 +47,54 @@ def listen_to_requests():
         except KeyboardInterrupt:
             break
 
-def handle_roundrobin(jobs,workers):
-    #jobs is structured like: [{'0': [[('0_M0', 2)], [('0_R0', 4), ('0_R1', 1)]]}, {'1': [[('1_M0', 1)], [('1_R0', 2), ('1_R1', 4)]]}]
+
+def handle_roundrobin(jobs, workers):
+    # jobs is structured like: [{'0': [[('0_M0', 2)], [('0_R0', 4), ('0_R1', 1)]]}, {'1': [[('1_M0', 1)], [('1_R0', 2), ('1_R1', 4)]]}]
     while(jobs):
-        task=getTask(jobs)
-        worker_found=False
+        task = getTask(jobs)
+        worker_found = False
         while(not worker_found):
             for w in workers:
                 if(w["free_slots"]):
-                    worker_found=True
-                    worker_id=w["worker_id"]        #worker_id
-                    w["free_slots"]-=1
-                    #task -> workers[id-1]
-                    #w[free_slots]++ after job is completed
+                    worker_found = True
+                    worker_id = w["worker_id"]  # worker_id
+                    w["free_slots"] -= 1
+                    # task -> workers[id-1]
+                    # w[free_slots]++ after job is completed
                     break
 
 
-
-
-def handle_random(jobs,workers):
+def handle_random(jobs, workers):
     while(jobs):
-        task=getTask(jobs)
-        worker_found=False
-        choose=[1,2,3]
+        task = getTask(jobs)
+        worker_found = False
+        choose = [1, 2, 3]
         while(not worker_found):
-            worker_id=random.choice(choose)
+            worker_id = random.choice(choose)
             if(workers[worker_id-1]["free_slots"]):
-                worker_found=True
-                workers[worker_id-1]["free_slots"]-=1
-                #task -> workers[id-1]
-                #w[free_slots]++ after job is completed
+                worker_found = True
+                workers[worker_id-1]["free_slots"] -= 1
+                # task -> workers[id-1]
+                # w[free_slots]++ after job is completed
                 break
-
-
 
 
 def handle_LL():
     while(jobs):
-        task=getTask(jobs)
-        worker_found=False
-        max_slots=0
-        max_id=0
-        while(not worker_found):        #add wait clause if no slots are free?
+        task = getTask(jobs)
+        worker_found = False
+        max_slots = 0
+        max_id = 0
+        while(not worker_found):  # add wait clause if no slots are free?
             for w in workers:
-                if(w["free_slots"]>max_slots):
-                    max_slots=workers[w]["free_slots"]
-                    max_id=w["worker_id"]
+                if(w["free_slots"] > max_slots):
+                    max_slots = workers[w]["free_slots"]
+                    max_id = w["worker_id"]
 
             if(workers[max_id-1]["free_slots"]):
-                worker_found=True
-                workers[max_id-1]["free_slots"]-=1
-                #task -> workers[max_id-1]
+                worker_found = True
+                workers[max_id-1]["free_slots"] -= 1
+                # task -> workers[max_id-1]
 
 
 def listen_to_workers():
@@ -99,17 +102,15 @@ def listen_to_workers():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind(('localhost', 5001))
     s.listen(1)
-    
+
     while(1):
         try:
-            host, _ = s.accept()    
+            host, _ = s.accept()
             with host:
                 data = host.recv(1024)
                 print(data.decode())
         except KeyboardInterrupt:
             break
-
-
 
 
 if __name__ == '__main__':
@@ -137,27 +138,31 @@ if __name__ == '__main__':
 
         msg = "Hello from master"
         sockets[worker["worker_id"]].send(msg.encode())
-    
+
     print("Connection with Workers successful!!\n")
 
-    workers=[x for x in data["workers"]] #list of dictionaries, each dictionary contains the worker details.
+    # list of dictionaries, each dictionary contains the worker details.
+    workers = [x for x in data["workers"]]
     for i in workers:
-        i["free_slots"]=i["slots"]
-        #print(i)
+        i["free_slots"] = i["slots"]
+        # print(i)
 
-    
+    d = "task_01,5"
+    for worker in data["workers"]:
+        send_tasks(d, worker["port"])
+
     requests_listener = threading.Thread(target=listen_to_requests)
     worker_listener = threading.Thread(target=listen_to_workers)
     requests_listener.start()
     worker_listener.start()
 
     print("Continue processing on the master thread to assign jobs")
-    if algo == "RR":
+    '''if algo == "RR":
         handle_roundrobin()
     elif algo == "RANDOM":
         handle_random()
     elif algo == "LL":
-        handle_LL()
+        handle_LL()'''
 
     worker_listener.join()
     requests_listener.join()
